46d06c6bcaafc8e90ab3f3bf52cb376693484d05
mark jones <markjones@eircom.net>
2020-06-09 14:28:49 +0100

    Add Mock NotificationCentre object

--- TimerControlTests/Mocks/MockNotificationCentre.swift
+++ TimerControlTests/Mocks/MockNotificationCentre.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class MockNotificationCentre: NotificationCenter {
+    struct MockObserver {
+        var observer: Any
+        var selector: Selector
+        var name: NSNotification.Name?
+        var object: Any?
+    }
+
+    var Observers: [MockObserver] = []
+
+    override func addObserver(_ observer: Any,
+                              selector aSelector: Selector,
+                              name aName: NSNotification.Name?,
+                              object anObject: Any?) {
+        Observers.append(MockObserver(observer: observer,
+                                      selector: aSelector,
+                                      name: aName,
+                                      object: anObject))
+    }
+}
