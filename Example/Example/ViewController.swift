fe69f0e79071e8164f4cea20fe3d440e4a170a52
mark jones <markstephenjones@outlook.com>
2020-07-16 18:03:02 +0100

    Added configuration button with preset configurations to Example application

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -14,15 +14,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var secondsBox: UITextField!
     @IBOutlet var timerControl: TimerControlView!
     var VCCounter: Int = 0
+    var randomiser = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
         timerControl.delegate = self
-        timerControl.configureTimerControl(innerColor: .blue,
-                                           outerColor: .black,
-                                           counterTextColor: .white,
-                                           arcWidth: 3,
-                                           arcDashPattern: .narrow)
         self.secondsBox.delegate = self
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
@@ -36,6 +33,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
         } else {
             timerControl.startTimer(duration: Int(10.0))
         }
+    }
+
+    @IBAction func configure() {
+        let innerColor: [UIColor] = [.blue, .systemGray3, .systemGray5, .green]
+        let outerColor: [UIColor] = [ .black, .red, .orange, .magenta]
+        let textColor: [UIColor] = [.white, .magenta, .systemBlue, .black]
+        let hide: [Bool] = [false, false, false, true]
+        let arcWidths: [Int] = [3, 4, 5, 6]
+        let dashPatterns: [TimerControlDashPattern] = [.narrow, .medium, .wide, .none]
+
+        timerControl.configureTimerControl(innerColor: innerColor[randomiser%4],
+                                           outerColor: outerColor[randomiser%4],
+                                           counterTextColor: textColor[randomiser%4],
+                                           hideInactiveCounter: hide[randomiser%4],
+                                           arcWidth: arcWidths[randomiser%4],
+                                           arcDashPattern: dashPatterns[randomiser%4])
+        randomiser += 1
     }
 
     @IBAction func resetSleep() {
